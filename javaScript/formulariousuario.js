5abc13aa84d84ac02978dc8617eeb0c7840c0ead
tammyrpd <46050192+tammyrpd@users.noreply.github.com>
2019-01-17 21:56:48 -0300

    Add files via upload
    javaScript

---
+++ javaScript/formulariousuario.js
@@ -0,0 +1,16 @@
+﻿/*formulario agregar usuario*/
+   <script>
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+                });
+                });
+
+        $(document).ready(function () {
+            $('#formus').hide();
+            $('#agregarus').click(function (e) {
+                e.preventDefault();
+                $('#formus').show();
+                })
+                })
+    </script>
