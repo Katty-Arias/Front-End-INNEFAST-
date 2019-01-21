9b98c7cf73b4fe12cf80584b2b480e1c37311eb3
francisco <Francisco20045@live.cl>
2019-01-21 13:01:44 -0300

    JavaScript del login

---
+++ js/loginAngular.js
@@ -0,0 +1,36 @@
+var app = angular.module('AppLogin', []);
+
+app.controller('ctrlLogin', function($scope, $http)
+{    
+    
+    $scope.mostrarLogin = true;
+    $scope.mostrarError = false;
+    $scope.mensaje = "";
+
+    $scope.iniciarSesion = function(){
+
+        if( $scope.modelUsuario == "" &&  $scope.modelPassword=="" ){
+            $scope.mensajedatos = "Ingrese datos";
+            return ;
+        }
+        
+        if( $scope.modelUsuario == "admin" &&  $scope.modelPassword=="123456" ){
+        	 $scope.mostrarError = false;
+            $scope.mostrarLogin = false;
+             $state.go('/home.html');
+
+ 
+
+/*            $scope.mostrarError = false;
+            $scope.mostrarLogin = false;
+  */          
+
+            //
+        }else{
+            $scope.mostrarError = true;
+            $scope.mostrarLogin = true;
+            $scope.mensaje = "Usuario y/o Clave incorrectos";
+        }
+        
+    };
+});
