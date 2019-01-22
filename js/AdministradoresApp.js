e2fd13e24b3b3b0569368c3dd71682019ec93b97
tammyrpd <46050192+tammyrpd@users.noreply.github.com>
2019-01-22 20:51:35 -0300

    agregar angular a tablas

---
+++ js/AdministradoresApp.js
@@ -0,0 +1,19 @@
+﻿var app = angular.module('AdministradoresApp', []);
+
+app.controller('ctrlAdministradores', function ($scope, $http) {
+    $scope.guardar = function () {
+        var administrador = {
+            "id": 121,
+            "nombre": $scope.nombre,
+            "cargo": $scope.cargo,
+            "rut": $scope.rut,
+          
+        };
+
+        $http.put("http://localhost:8080/administradores", administrador)
+            .then(function (resp) {
+                console.log(resp.data);
+                window.location.href = "visualAdministradores.html";
+            });
+    };
+});
