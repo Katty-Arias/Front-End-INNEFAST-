63848a6e99504ae3e8bc57d051ed6691f55b0876
tammyrpd <46050192+tammyrpd@users.noreply.github.com>
2019-01-22 21:10:08 -0300

    historialAPP, tabla historial

---
+++ js/HistorialApp.js
@@ -0,0 +1,21 @@
+﻿var app = angular.module('HistorialApp', []);
+
+app.controller('ctrlHistorial', function ($scope, $http) {
+    $scope.guardar = function () {
+        var administrador = {
+            "nombre": $scope.nombre,
+            "rut": $scope.rut,
+            "correo": $scope.correo,
+            "habilidades": $scope.habilidades,
+            "proyecto": $scope.proyecto,
+            
+          
+        };
+
+        $http.put("http://localhost:8080/historial", historial)
+            .then(function (resp) {
+                console.log(resp.data);
+                window.location.href = "visualHistorialDeProyectos.html";
+            });
+    };
+});
